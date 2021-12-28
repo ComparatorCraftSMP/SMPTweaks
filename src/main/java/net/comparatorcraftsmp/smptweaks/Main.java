d259b75c3e94b5d7e50ce7ad0bc2bbe8e23b2f33
Dallin Hoyt <dhhoyt2002@gmail.com>
2021-12-27 17:00:02 -0700

    inital commit

---
+++ src/main/java/net/comparatorcraftsmp/smptweaks/Main.java
@@ -0,0 +1,9 @@
+package net.comparatorcraftsmp.smptweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+     public void onEnable() {
+        System.out.println("Huzzah!");
+     }
+}
